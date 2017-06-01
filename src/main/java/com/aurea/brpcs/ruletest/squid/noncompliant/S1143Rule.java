9758acb177d6320f02cfec4079c2b3af8fa04dc0
Alexandre <ajanoni@gmail.com>
2017-06-01 19:48:42 -0300

    SQUID S1143 cases

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1143Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1143Rule.java
@@ -4,18 +4,49 @@ public class S1143Rule {
 
 	public static void main(String[] args) {
 		try {
-			doSomethingWhichThrowsException();
+			case1();
 			System.out.println("OK");
 		} catch (RuntimeException e) {
 			System.out.println("ERROR");
 		}
 	}
 
-	public static void doSomethingWhichThrowsException() {
+	public static void case1() {
 		try {
 			throw new RuntimeException();
 		} finally {
 			return;                                        // Non-Compliant - prevents the RuntimeException from being propagated
+		}
+	}
+
+
+	public static void case2() {
+		try {
+			throw new RuntimeException();
+		} finally {
+			throw new Exception()                          // Non-Compliant - prevents the RuntimeException from being propagated
+		}
+	}
+
+	public static void case3() {
+		while(x<10) {
+			try {
+				x++;
+				throw new RuntimeException();
+			} finally {
+				continue;                          // Non-Compliant - prevents the RuntimeException from being propagated
+			}
+		}
+	}
+
+	public static void case4() {
+		while(x<10) {
+			try {
+				x++;
+				throw new RuntimeException();
+			} finally {
+				break;                          // Non-Compliant - prevents the RuntimeException from being propagated
+			}
 		}
 	}
 }
