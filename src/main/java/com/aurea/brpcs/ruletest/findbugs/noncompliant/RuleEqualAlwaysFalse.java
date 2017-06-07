7e269ab7a56bac4c06bb70c20f47d908fe29dcbb
Ali Kemal Tasci <alikemaltasci@Ali-Tasci-MacBook-Pro.local>
2017-06-07 18:15:50 +0300

    BRPCS-134 - Implement Rule EQ_ALWAYS_FALSE : equals method always returns false

--- src/main/java/com/aurea/brpcs/ruletest/findbugs/noncompliant/RuleEqualAlwaysFalse.java
+++ src/main/java/com/aurea/brpcs/ruletest/findbugs/noncompliant/RuleEqualAlwaysFalse.java
@@ -2,13 +2,28 @@ package com.aurea.brpcs.ruletest.findbugs.noncompliant;
 
 public class RuleEqualAlwaysFalse {
 
-    @Override
-    public boolean equals(Object obj) {
-        return false;
+    public static class WithAnnotation {
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
     }
 
-    @Override
-    public int hashCode() {
-        return 0;
+    public static class WithOutAnnotation {
+
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
     }
 }
