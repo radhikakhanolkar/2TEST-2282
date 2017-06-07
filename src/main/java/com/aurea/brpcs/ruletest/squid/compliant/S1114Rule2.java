42f41ae75108fafb11de882d72ce2b794dd53c81
daniel.natali <daniel.natali@aurea.com>
2017-06-07 15:54:09 -0300

    Change to non-compliant

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S1114Rule2.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S1114Rule2.java
@@ -4,11 +4,11 @@ public class S1114Rule2 extends AbstractS1114Rule {
 
 	protected void finalize() throws Throwable {
 		try {
-			
+			doSomething();
 		} catch (Throwable t) {
 			releaseSomeResources();
-			super.finalize(); //compliant- Super is at the end of the method.
 		} 
+		super.finalize(); //compliant- Super is at the end of the method.
 	}
 
 	public void releaseSomeResources() {
