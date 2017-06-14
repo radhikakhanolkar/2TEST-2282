29bc3db422157ea5c1903aa764a9a3490f4e7e10
Evgeny Batalov <batalov.evgeny@gmail.com>
2017-06-14 13:58:17 +0300

    update

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2162Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2162Rule.java
@@ -14,13 +14,8 @@ public class S2162Rule extends Food {
         if (obj == null) {
             return false;
         }
-        if (S2162Rule.class == obj.getClass()) { // Noncompliant; broken for child classes
-            return ripe.equals(((S2162Rule) obj).getRipe());
-        }
-        if (obj instanceof S2162Rule) {  // Noncompliant; broken for child classes
-            return ripe.equals(((S2162Rule) obj).getRipe());
-        } else if (obj instanceof Season) { // Noncompliant; symmetry broken for Season class
-            // ...
+        if (this.getClass() == obj.getClass()) {
+            return ripe.equals(((S2162Rule)obj).getRipe());
         }
         return false;
     }
--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2162Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2162Rule.java
@@ -14,12 +14,16 @@ public class S2162Rule extends Food {
         if (obj == null) {
             return false;
         }
-        if (this.getClass() == obj.getClass()) {
-            return ripe.equals(((S2162Rule)obj).getRipe());
+        if (S2162Rule.class == obj.getClass()) { // Noncompliant; broken for child classes
+            return ripe.equals(((S2162Rule) obj).getRipe());
+        }
+        if (obj instanceof S2162Rule) {  // Noncompliant; broken for child classes
+            return ripe.equals(((S2162Rule) obj).getRipe());
+        } else if (obj instanceof Season) { // Noncompliant; symmetry broken for Season class
+            // ...
         }
         return false;
     }
-
     public Season getRipe() {
         return ripe;
     }
