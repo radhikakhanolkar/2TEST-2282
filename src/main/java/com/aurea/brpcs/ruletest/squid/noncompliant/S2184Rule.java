54db7ad5537a77665380c7deac6634425155009b
Alexandre <ajanoni@gmail.com>
2017-06-26 19:18:29 -0300

    S2184 Rule update

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2184Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2184Rule.java
@@ -12,7 +12,7 @@ public class S2184Rule {
     long bigNum = Integer.MAX_VALUE + 2; // Noncompliant. Yields -2147483647
     long bigNegNum = Integer.MIN_VALUE - 1; //Noncompliant, gives a positive result instead of a negative one.
 
-    long seconds = 2147483647;
+    long seconds = 2147483;
     Date myDate = new Date(seconds * 1_000); //Noncompliant, won't produce the expected result if seconds > 2_147_483_647
 
     public long compute(int factor) {
