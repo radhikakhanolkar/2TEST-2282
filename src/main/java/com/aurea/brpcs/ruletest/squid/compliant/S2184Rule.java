5d0ee2001c400f786c467c1de9dd92a7f5417158
Alexandre <ajanoni@gmail.com>
2017-06-26 14:42:49 -0300

    S2184 Rule update

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2184Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2184Rule.java
@@ -6,31 +6,41 @@ public class S2184Rule {
 
     long seconds = 2147483;
 
-    float twoThirds = 2f/3; // 2 promoted to float. Yields 0.6666667
-    long millisInYear = 1_000L*3_600*24*365; // 1000 promoted to long. Yields 31_536_000_000
+    float twoThirdsFloat = 2f / 3; // 2 promoted to float. Yields 0.6666667
+    float multFloat = 2f * 3;
+    double twoThirdsDouble = 2d / 3;
+    double multiDouble = 2d * 3;
+    long millisInYear = 1_000L * 3_600 * 24 * 365; // 1000 promoted to long. Yields 31_536_000_000
     long bigNum = Integer.MAX_VALUE + 2L; // 2 promoted to long. Yields 2_147_483_649
-    long bigNegNum =  Integer.MIN_VALUE-1L; // Yields -2_147_483_649
+    long bigNegNum = Integer.MIN_VALUE - 1L; // Yields -2_147_483_649
     Date myDate = new Date(seconds * 1_000L);
 
-    float twoThirds2 = (float)2/3; // 2 cast to float
-    long millisInYear2 = (long)1_000*3_600*24*365; // 1_000 cast to long
-    long bigNum2 = (long)Integer.MAX_VALUE + 2;
-    long bigNegNum2 =  (long)Integer.MIN_VALUE-1;
-    Date myDate2 = new Date((long)seconds * 1_000);
+    float twoThirds2Float = (float) 2 / 3; // 2 cast to float
+    float multFloat2 = (float) 2 * 3;
+    double twoThirdsDouble2 = (double) 2 / 3; // Noncompliant; int division. Yields 0.0
+    double multiDouble2 = (double) 2 * 3;
+    long millisInYear2 = (long) 1_000 * 3_600 * 24 * 365; // 1_000 cast to long
+    long bigNum2 = (long) Integer.MAX_VALUE + 2;
+    long bigNegNum2 = (long) Integer.MIN_VALUE - 1;
+    Date myDate2 = new Date((long) seconds * 1_000);
 
-    public long compute(int factor){
+    public long compute(int factor) {
         return factor * 10_000L;
     }
 
-    public float compute2(long factor){
+    public float compute2(long factor) {
         return factor / 123f;
     }
 
-    public long compute(long factor){
+    public long compute(long factor) {
         return factor * 10_000;
     }
 
-    public float compute2(float factor){
+    public float compute2(float factor) {
+        return factor / 123;
+    }
+
+    public double compute3(double factor) {
         return factor / 123;
     }
 
--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2184Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2184Rule.java
@@ -4,19 +4,26 @@ import java.util.Date;
 
 public class S2184Rule {
 
-    float twoThirds = 2 / 3; // Noncompliant; int division. Yields 0.0
+    float twoThirdsFloat = 2 / 3; // Noncompliant; int division. Yields 0.0
+    float multiFloat = 2 * 3;
+    double twoThirdsDouble = 2 / 3; // Noncompliant; int division. Yields 0.0
+    double multiDouble = 2 * 3;
     long millisInYear = 1_000 * 3_600 * 24 * 365; // Noncompliant; int multiplication. Yields 1471228928
     long bigNum = Integer.MAX_VALUE + 2; // Noncompliant. Yields -2147483647
     long bigNegNum = Integer.MIN_VALUE - 1; //Noncompliant, gives a positive result instead of a negative one.
 
-    long seconds = 2147483;
-    Date myDate = new Date(seconds * 1_000); //Noncompliant, won't produce the expected result if seconds > 2_147_483
+    long seconds = 2147483647;
+    Date myDate = new Date(seconds * 1_000); //Noncompliant, won't produce the expected result if seconds > 2_147_483_647
 
     public long compute(int factor) {
         return factor * 10_000;  //Noncompliant, won't produce the expected result if factor > 214_748
     }
 
     public float compute2(long factor) {
+        return factor / 123;  //Noncompliant, will be rounded to closest long integer
+    }
+
+    public double compute3(long factor) {
         return factor / 123;  //Noncompliant, will be rounded to closest long integer
     }
 
