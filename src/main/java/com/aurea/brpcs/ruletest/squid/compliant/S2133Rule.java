3e132fec7dcd0c69b9cdc96a088eb261451c6fee
Alexandre <ajanoni@gmail.com>
2017-07-03 10:19:57 -0300

    S2133 Rule update

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2133Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2133Rule.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 public class S2133Rule {
 
+    Date date2 = new Date();
+
     public void testGetClass() {
         Class c = Date.class;
     }
@@ -15,8 +17,16 @@ public class S2133Rule {
 
         Date date1 = new Date();
         date1.setTime(1000L);
+        date2.setTime(1000L);
+        testMethodDate(date2);
         if(date1.getClass() == Date.class){
             testMethodDate(date1);
+        }
+    }
+
+    public void test2(){
+        if(date2.getClass() == Date.class){
+            System.out.println(date2);
         }
     }
 
