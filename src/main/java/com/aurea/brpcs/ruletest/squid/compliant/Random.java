37195e56c7b38a3fdd452d54b1a089d309199146
Michal Gorniewski <michal.gorniewski@aurea.com>
2017-07-03 16:18:20 +0200

    S2245 Rule

---
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/Random.java
@@ -0,0 +1,10 @@
+package com.aurea.brpcs.ruletest.squid.compliant;
+
+class Random {
+
+    private final int value;
+
+    Random(int value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2245Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2245Rule.java
@@ -1,7 +1,5 @@
 package com.aurea.brpcs.ruletest.squid.compliant;
 
-import com.aurea.brpcs.ruletest.helpers.Random;
-
 public class S2245Rule {
 
     private final Random r = new Random(12);
