61b53960fb3f22cb3396cefff6f63536b92f1047
Evgeny Batalov <batalov.evgeny@gmail.com>
2017-07-04 15:34:43 +0300

    exception
    ImpossibleDownCastToArray exception

--- src/main/java/com/aurea/brpcs/ruletest/findbugs/compliant/ImpossibleDowncastOfToArray.java
+++ src/main/java/com/aurea/brpcs/ruletest/findbugs/compliant/ImpossibleDowncastOfToArray.java
@@ -1,10 +1,15 @@
 package com.aurea.brpcs.ruletest.findbugs.compliant;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class ImpossibleDowncastOfToArray {
 
     String[] getAsArray(Collection c) {
         return (String[])c.toArray(new String[c.size()]);
+    }
+
+    String[] getAsArray1(Collection c) {
+        return (String[]) Arrays.asList(new String[] { "a" }).toArray();
     }
 }
