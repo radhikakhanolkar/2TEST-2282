66d991a4c5ff83133c4231be8e9755b9e1152707
Evgeny Batalov <batalov.evgeny@gmail.com>
2017-07-21 07:21:00 +0300

    update with findbugs REC_CATCH_EXCEPTION Exception is caught when Exception is not thrown - add case

--- src/main/java/com/aurea/brpcs/ruletest/findbugs/compliant/REC_CATCH_EXCEPTION.java
+++ src/main/java/com/aurea/brpcs/ruletest/findbugs/compliant/REC_CATCH_EXCEPTION.java
@@ -12,4 +12,13 @@ public class REC_CATCH_EXCEPTION {
             throw (LinkageError) new LinkageError("Failed to resolve " + c).initCause(x);
         }
     }
+
+    private void resolve2(Class c) {
+        try {
+            c.getGenericSuperclass();
+            throw new Exception("hello");
+        } catch (Exception x) {
+            throw (LinkageError) new LinkageError("Failed to resolve " + c).initCause(x);
+        }
+    }
 }
