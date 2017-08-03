8f97049ede5ccf748651e02321af5cf20441c371
Alexandre <ajanoni@gmail.com>
2017-08-03 09:21:38 -0300

    groovy scripts

--- groovy/src/sonar.groovy
+++ groovy/src/sonar.groovy
@@ -78,7 +78,10 @@ queryDM_NUMBER_CTOR = "MATCH (newInstance:ClassInstanceCreation)-[:type]->(type:
         "ORDER BY newInstance.file,newInstance.line\n"
 
 
-cgProjects = [
+queryS1116 = "MATCH (n:EmptyStatement)\n" +
+        "RETURN DISTINCT n.col as col, n.line as line, n.file as file"
+
+//cgProjects = [
 //        'business-payment'                        : 'a98c9c54-23c1-4dfc-a9d1-5335c1368af3',
 //        'versata-m1.ems'                          : 'e494eb5f-5a45-4259-b2a7-714f16dbd6b1',
 //        'aurea-sonic-mq'                          : '5beea8ba-c579-46f8-8c9e-c94124a4f12e',
@@ -89,18 +92,121 @@ cgProjects = [
 //        'kerio-mykerio-kmanager'                  : 'cb7cd6df-a193-444f-8a17-633da0025a18',
 //        'aurea-lyris-platform-edge'               : '34267f92-0883-4004-bd33-1c570ab54552',
 //        'devfactory-codegraph-server'             : '0db9b092-2e84-438e-949e-5abaad903dad',
-        'aurea-java-brp-cs-ruletest'              : '9a3f2b9b-3bfa-4b99-b363-f1d0659fa778',
-        'org.jenkins-ci.main:pom'                 : '15e366aa-fc18-4ea0-bec5-9443a2a7a8f4',
-        'hibernate-orm'                           : 'f65e663c-ab46-44e2-be13-03d84eda1bf3',
-        'org.apache.wicket:wicket-parent'         : '37821c2d-f736-46bc-b1a4-fa2400dab0e3',
-        'org.apache.struts:struts2-parent'        : 'f31fed00-936f-47ae-8f83-71e6e223b8a8',
-        'org.springframework:spring'              : 'ce1ab499-2bc4-49c8-adf9-8bf1f4e2e1e7',
-        'org.apache.hive:hive'                    : '68151e65-78ab-46f8-aa28-7e90ef6168f8',
-        'org.drools:drools'                       : 'b6546cab-7dd2-4748-99af-738658c7b5d7',
-        'org.apache.activemq:activemq-parent'     : 'ea4b9d5a-35d3-4810-b605-7f7983062ff9',
-        'org.eclipse.jetty:jetty-project'         : '1def6b67-e748-43e4-90a8-d9627e6688c6',
-        'org.eclipse.jgit:org.eclipse.jgit-parent': '43c578b3-bd45-4fee-8ba4-1acfdb449ab8'
+//        'aurea-java-brp-cs-ruletest'              : '9a3f2b9b-3bfa-4b99-b363-f1d0659fa778',
+//        'org.jenkins-ci.main:pom'                 : '15e366aa-fc18-4ea0-bec5-9443a2a7a8f4',
+//        'hibernate-orm'                           : 'f65e663c-ab46-44e2-be13-03d84eda1bf3',
+//        'org.apache.wicket:wicket-parent'         : '37821c2d-f736-46bc-b1a4-fa2400dab0e3',
+//        'org.apache.struts:struts2-parent'        : 'f31fed00-936f-47ae-8f83-71e6e223b8a8',
+//        'org.springframework:spring'              : 'ce1ab499-2bc4-49c8-adf9-8bf1f4e2e1e7',
+//        'org.apache.hive:hive'                    : '68151e65-78ab-46f8-aa28-7e90ef6168f8',
+//        'org.drools:drools'                       : 'b6546cab-7dd2-4748-99af-738658c7b5d7',
+//        'org.apache.activemq:activemq-parent'     : 'ea4b9d5a-35d3-4810-b605-7f7983062ff9',
+//        'org.eclipse.jetty:jetty-project'         : '1def6b67-e748-43e4-90a8-d9627e6688c6',
+//        'org.eclipse.jgit:org.eclipse.jgit-parent': '43c578b3-bd45-4fee-8ba4-1acfdb449ab8'
+//]
+
+cgProjects = [
+        'internal:TestControlMsBuild':'e9c995a4-51e2-431f-94ea-adfd1f41c1f1',
+        'ignite-acorn-aaa':'3c4c53bc-10d3-49fb-a10e-d40a0be96b71',
+        'ignite-acorn-pa5g':'c469b40d-437e-4993-8832-722bf28b9051',
+        'aurea-nextdocs-nextdocs61x':'a0715165-e0db-49b3-8d2a-a55cc03e7d80',
+        'dotnettracker':'8683f7e3-c161-4331-986f-980f0673c40f',
+        'gfi-mail-archiver':'67998330-2e6a-4df6-b7f3-336d82e6e346',
+        'gfi-directory-service':'dca7bc04-c280-4f3a-b06e-100ced06d40e',
+        'gfi-endpointsecurity':'29be77c9-cd91-428b-a562-2f7ccd04f90f',
+        'gfi-eventsmanager':'e0834782-5ae4-47a9-a8ee-a7fb1c03cfc6',
+        'gfi-faxmaker-online':'3042f932-6ce2-4f95-84ac-351c31ddde66',
+        'gfi-languard':'2937c715-ee77-4b2e-8b86-f19870d6f551',
+        'gfi-mail-essentials':'7784c65e-9cb3-4d30-afd3-816400fbf4a4',
+        'gfi-oneconnect-M1-EMSClient':'4abca461-1195-4dc2-b98a-84cedacdca23',
+        'gfi-oneguard':'1a3a9cb4-c2d0-4e46-85dd-b29bc32112c5',
+        'gfi-web-monitor':'561f1ad1-ca8e-49a9-8dd1-4c270bc8412e',
+        'dotnet-importer':'cdd0f135-f158-434b-9ea2-0d290a4044c9',
+        'versata-m1.client-ems-client':'0ee87265-a7a7-4723-8f86-7068fea5ff6b',
+        'leadlander':'f476cbdd-ba5a-45e0-bbba-d72fe55e30c5',
+        'filebound_v7':'0fc5447a-0871-46d4-9f16-6522ae893bde',
+        'filebound_v7':'50b1b37d-3e3f-44b0-be9a-fa31f4003a2e',
+        'projectlviv':'0ffab9a2-a572-413b-a756-093a50072871',
+        'filebound_capture_v7':'0b0da000-ddbd-465f-bd64-ade9bfdf5970',
+        'eclipse-tfs-2-git':'bbf3ec77-dbb7-4894-b84a-064018952a76',
+        'vepmlive-epmlive2013release':'97b7e16e-6725-4005-b381-a9114e672d45',
+        'MvcContosoUniversity':'adbfd587-3e57-476c-9bfb-608814054fd7',
+        'Nancy':'69c843ee-2801-469b-99c8-fa7cc1ad2406',
+        'netmq':'074bd65f-b176-488a-9c4d-e871e0db6681',
+        'Newtonsoft.Json':'d18b5701-90ca-49cf-b3d3-64e4b1af75fe',
+        'nhibernate-core':'196fddf1-fe19-4310-be5a-1936147c6aa9',
+        'nunit':'cba76385-80c9-4b81-ba67-d247f0c4092d',
+        'RestSharp':'fbf4cc37-3ad5-4412-a896-3dc9829b7df8',
+        'Simple.Data':'ac98319b-c897-4ab6-a909-be933334a804',
+        'MVCBlog':'c37386b7-9b0e-4591-a7d1-fb12ae9e3997',
+        'CsvHelper':'4ce67a69-8145-40e3-bd15-bb30eb80de78',
+        'Rx.NET':'e9727372-138b-4bea-8570-5b50012a42c6',
+        'NSwag':'0b7a6707-56e9-43a8-94bc-d090745d0c12',
+        'fluent-nhibernate':'bcfbfa29-4e78-497b-a48a-7184bef10b8b',
+        'mongo-csharp-driver':'dcc6defe-290d-427e-9154-56349744cb7c',
+        'refit':'4291bfcc-a687-4063-afc0-6ca7f29bb7c6',
+        'ModernHttpClient':'71633292-67b7-46d6-9d3a-10fab17d34c0',
+        'ravendb':'c93b957c-255d-4c68-a97e-65d9e88e1597',
+        'LiteDB':'a6364663-e24b-4526-ab5e-237942214475',
+        'Microsoft.Data.Sqlite':'9f0f2a3f-8a87-40ba-87f3-b9f4c0fdcae3',
+        'EntityFramework6':'d48ba663-841c-4235-8da4-c256d789c450',
+        'Razor':'d85f0a2f-8ee0-4088-af4f-43594c8e4478',
+        'dotnet-watch':'361aac07-a831-43b6-ad36-6e8fb56e60c1',
+        'MusicStore':'5e55bdcb-e93a-452a-b95d-7bfdba6080b1',
+        'NerdDinner':'8291c2b1-df82-42c1-90d0-c184592bf78f',
+        'Nancy.Blog':'fa0bbd4d-3abe-404d-9eb7-bb42f5e1605c',
+        'Nancy.Demo.Samples':'e9196e5c-07b5-4536-b67e-d39bd14ad393',
+        'Avalonia':'b1e3fde7-f7fc-4f9d-921e-a141a0a88fba',
+        'dapper-dot-net':'bb26ef47-217f-4dd0-909c-6cf427dc2abc',
+        'AutoMapper':'c248945c-f95c-4907-964f-2b4ca93995ac',
+        'PushSharp':'299f63b6-1a9e-4239-acda-9c67a3637a40',
+        'FastColoredTextBox':'0ced836e-7ef3-4c24-baed-83a83413f5f5',
+        'SignalR':'af142301-7fa6-462f-a9a7-c146aafe6521',
+        'Opserver':'287b4e2a-e79a-4875-b5f3-e790e9632b49',
+        'ReactiveUI':'14ab776a-53a1-400e-9787-9e50003383b5',
+        'CefSharp':'7bd89d33-ff9d-4962-a5f3-5faeb5e2522a',
+        'NLog':'36cfba3a-2b54-4358-aca4-39502abc381c',
+        'Hangfire':'999b3eb7-1dde-4698-86d1-ad47039f64c7',
+        'Massive':'a95ae622-187f-4842-8123-101dca67d23f',
+        'FluentValidation':'7633a2d5-ff9f-4fa8-8278-df8371e8a648',
+        'StackExchange.Redis':'459019d3-126d-44db-b7c3-4bf6be489955',
+        'quartznet':'a7e66f6b-477c-42bd-a1a1-9067664cb673',
+        'Topshelf':'c83e54d2-d368-40c1-b409-86424588beaa',
+        'Jil':'f6a2e4a8-6d46-49de-b2a3-f368acb85c96',
+        'fluentmigrator':'826efab7-dc6f-4311-982b-244bdbeb6936',
+        'Akavache':'6dfb4ba5-43b3-4644-8f93-711f29570025',
+        'BenchmarkDotNet':'109bce79-6bbe-4815-8acf-30799cdc8ede',
+        'RazorEngine':'745e801a-0dec-4a62-9140-7161f8b8a4cf',
+        'ServiceStack.Redis':'0ad20cbd-1b93-44b4-ba39-9d7067beee6d',
+        'KestrelHttpServer':'1020141d-9e08-4cca-8412-a831c56fdf83',
+        'Orchard':'fb3cf62e-e200-43b2-9839-c1ef27e355b5',
+        'NEventStore':'15e480e6-f165-4e88-98de-b9f37bb47691',
+        'MailKit':'9a3a899e-20ce-47cf-822b-d1a4432ec6c1',
+        'Emby':'56c1f882-8743-4485-8010-45a583262561',
+        'protobuf-net':'1df14255-4678-400a-8860-117bbe732dcb',
+        'npgsql':'86b28c26-28fa-4ce0-ad87-7b94f2cc43e4',
+        'PetaPoco':'aff8e90f-7113-44aa-989f-62ce0c38a244',
+        'EasyNetQ':'c7b4dcd5-56cb-40f0-9fe2-8b106d2a56ba',
+        'ServiceStack.Text':'5508a0ab-ac43-46f0-9353-313bae126b2a',
+        'abot':'f18b72e2-bc18-460c-8cd3-929273414fa3',
+        'MiniBlog':'92bb1556-89a0-4398-95ec-d6b129790a05',
+        'nopCommerce':'e74f4e40-5c5d-4913-9a09-8ac5302088bc',
+        'oxyplot':'b045cb40-d85e-4f76-b7b9-e3f57822b6d1',
+        'SocialGoal':'56f26d1f-1261-4c22-b2ea-0ac5152b9a7e',
+        'Live-Charts':'48378976-d14d-4806-9e5f-6781272ce5ec',
+        'SmartStoreNET':'0109f83b-e203-4af7-9fb6-75d53061caf1',
+        'MvcMailer':'518e58dd-5d95-4fe8-9dff-8d5951cf2d28',
+        'NetGain':'e6b8b461-b91d-4b95-b0c6-8991f67fb06f',
+        'FluentScheduler':'b5533dc7-3a1d-424d-aef6-6e69985b908c',
+        'StyleCopAnalyzers':'898d4569-d1e2-45f8-8c90-60c46bff3ad0',
+        'CommonMark.NET':'2ba2aba6-fb61-480b-8052-62343660dd68',
+        'pascalabcnet':'fbf96395-3e10-42ea-a0e3-62b1f3d58581',
+        'PowerPointLabs':'b8a9d954-aae6-4e9d-bb91-614402ae9329',
+        'NAudio':'488936c7-4271-444a-b893-f71b5f9c6aaf',
+        'logging-log4net':'d22b7ff2-5e77-4211-8ae6-5944cbf6ac0d'
 ]
+
+
 
 String.metaClass.encodeURL = {
     java.net.URLEncoder.encode(delegate, "UTF-8")
@@ -125,15 +231,21 @@ fileLocal = "/Users/ajanoni/sonarcsv"
 sonarBaseUrl = "http://brp-sonar.ecs.devfactory.com"
 
 //ruleId = "rules=squid%3AS1210" //CHANGE THE RULE NAME HERE
-ruleId = "rules=findbugs%3ADM_NUMBER_CTOR"
+//ruleId = "rules=findbugs%3ADM_NUMBER_CTOR"
 
+//sonarUrl = sonarBaseUrl + "/api/issues/search?" + componentRoots(cgProjects) + "&" + ruleId
+
+sonarBaseUrl = "http://netbrp-sonarqube-stg.ecs.devfactory.com/"
+ruleId = "rules=csharpsquid%3AS1116"
 sonarUrl = sonarBaseUrl + "/api/issues/search?" + componentRoots(cgProjects) + "&" + ruleId
 
-sonarViolation = toViolationDTO(findViolations())
+//sonarViolation = toViolationDTO(findViolations())
+//
+//cgViolation = getViolationFromCG(queryS1116) //CHANGE THE QUERY HERE
+//
+//exportToCsv("S1116", sonarViolation, cgViolation) //CHANGE THE QUERY HERE
 
-cgViolation = getViolationFromCG(queryDM_NUMBER_CTOR) //CHANGE THE QUERY HERE
-
-exportToCsv("DM_NUMBER_CTOR", sonarViolation, cgViolation) //CHANGE THE QUERY HERE
+generateCsvPerRule()
 
 Set<Violation> getViolationFromCG(String query) {
 
@@ -141,8 +253,8 @@ Set<Violation> getViolationFromCG(String query) {
 
     cgProjects.each { k, v ->
         def cgClient = new RESTClient('https://codegraph-api-prod.ecs.devfactory.com/api/1.0/graphs/' + v + '/query')
-        cgClient.getClient().params.setParameter("http.connection.timeout", 20000)
-        cgClient.getClient().params.setParameter("http.socket.timeout", 21000)
+        cgClient.getClient().params.setParameter("http.connection.timeout", 30000)
+        cgClient.getClient().params.setParameter("http.socket.timeout", 30000)
         println 'https://codegraph-api-prod.ecs.devfactory.com/api/1.0/graphs/' + v + '/query'
         retry(3, { e -> e.printStackTrace() }) {
             cgClient.request(Method.POST, ContentType.JSON) { req ->
@@ -176,7 +288,7 @@ void generateCsvPerRule() {
     violations.each {
         project, rule ->
             rule.each {
-                name, violation -> println "Total violations: ${violation.size()}"
+                name, violation -> println project + " : Total violations: ${violation.size()}"
             }
 
     }
@@ -303,7 +415,7 @@ void exportToCsv(Map<String, Map<String, List<String[]>>> projectRuleViolations)
             CSVWriter reportWriter = new CSVWriter(fileRbfWriter, (char) ",")
 
             List<String[]> violations = entryRuleViolation.getValue();
-            println " >>> " + violations.size()
+            println projectName + "," + violations.size()
             for (String[] item : violations) {
                 String[] reportLine = new String[3];
                 reportLine[0] = item[3]; // col
--- groovy/src/updateth.groovy
+++ groovy/src/updateth.groovy
@@ -21,22 +21,126 @@ import groovyx.net.http.RESTClient
  */
 
 
+//cgProjects = [
+//        'https://scm-ba.devfactory.com/scm/cap/business-payment.git'  : 'bef53d1aa446076ea0217d85ab89c99d99da7a3a',
+//        'https://github.com/trilogy-group/aurea-aes-edi.git'          : 'af3dc86',
+//        'https://github.com/trilogy-group/aurea-java-brp-cs-ruletest' : '',
+//        'https://github.com/trilogy-group/aurea-lyris-platform-edge'  : '15e8699d82de854f5e4d6c40fe137056afdd9854',
+//        'https://github.com/trilogy-group/aurea-sonic-mq'             : '2b8497b82efb8f2af5ba016a006d1349b224d9ea',
+//        'https://github.com/trilogy-group/devfactory-codegraph-server': 'fc3450cfa4fd1cce236daf0e4dbaf891104bd8bb',
+//        'https://github.com/trilogy-group/ignite-sensage-analyzer.git': 'f46a44ecbc5e95f778fb4f72cdb86509632b28d2',
+//        'https://github.com/trilogy-group/kerio-mykerio-kmanager'     : 'e40fd363d019e8ff29a90cbc974818abdd4dae7c',
+//        'https://github.com/trilogy-group/ta-smartleads-lms-mct.git'  : 'db45cdba9dce7cd97e721f954e1d7c1b3f0dbb6f',
+//        'https://github.com/trilogy-group/versata-m1.ems.git'         : '1045056ed830b808cca82ff4d06b4d9add50064c',
+//        'https://scm.devfactory.com/stash/scm/uppowersteering/pss.git': '18d5ec487055ce16a639bdd8efb7ab434ffba4bf',
+//        'https://github.com/trilogy-group/wicket.git'                 : '0889d9ec569ff8a3edf17881941e52e9155bbb65',
+//        'https://github.com/trilogy-group/drools.git'                 : '0a526a9876cc61374a50cf60a9c0bdf89e98ecd8',
+//        'https://github.com/trilogy-group/spring-framework.git'       : '88d3526ef1fcf0dc2bc9098159d98eaf472c245f'
+//]
+
+
 cgProjects = [
-        'https://scm-ba.devfactory.com/scm/cap/business-payment.git'  : 'bef53d1aa446076ea0217d85ab89c99d99da7a3a',
-        'https://github.com/trilogy-group/aurea-aes-edi.git'          : 'af3dc86',
-        'https://github.com/trilogy-group/aurea-java-brp-cs-ruletest' : '',
-        'https://github.com/trilogy-group/aurea-lyris-platform-edge'  : '15e8699d82de854f5e4d6c40fe137056afdd9854',
-        'https://github.com/trilogy-group/aurea-sonic-mq'             : '2b8497b82efb8f2af5ba016a006d1349b224d9ea',
-        'https://github.com/trilogy-group/devfactory-codegraph-server': 'fc3450cfa4fd1cce236daf0e4dbaf891104bd8bb',
-        'https://github.com/trilogy-group/ignite-sensage-analyzer.git': 'f46a44ecbc5e95f778fb4f72cdb86509632b28d2',
-        'https://github.com/trilogy-group/kerio-mykerio-kmanager'     : 'e40fd363d019e8ff29a90cbc974818abdd4dae7c',
-        'https://github.com/trilogy-group/ta-smartleads-lms-mct.git'  : 'db45cdba9dce7cd97e721f954e1d7c1b3f0dbb6f',
-        'https://github.com/trilogy-group/versata-m1.ems.git'         : '1045056ed830b808cca82ff4d06b4d9add50064c',
-        'https://scm.devfactory.com/stash/scm/uppowersteering/pss.git': '18d5ec487055ce16a639bdd8efb7ab434ffba4bf',
-        'https://github.com/trilogy-group/wicket.git'                 : '0889d9ec569ff8a3edf17881941e52e9155bbb65',
-        'https://github.com/trilogy-group/drools.git'                 : '0a526a9876cc61374a50cf60a9c0bdf89e98ecd8',
-        'https://github.com/trilogy-group/spring-framework.git'       : '88d3526ef1fcf0dc2bc9098159d98eaf472c245f'
+        'https://github.com/trilogy-group/ignite-acorn-aaa' : '',
+        'https://github.com/trilogy-group/ignite-acorn-pa5g' : '',
+        'https://scm.devfactory.com/stash/scm/dfidep/dfideplugins' : '',
+        'https://github.com/trilogy-group/aurea-nextdocs-nextdocs61x' : '',
+        'https://scm.devfactory.com/stash/scm/crossover/dotnettracker' : '',
+        'https://github.com/trilogy-group/gfi-mail-archiver' : '367857ad0ee4ba043be562fe25ed397e8f1064a7',
+        'https://github.com/trilogy-group/gfi-directory-service' : '23ea69e7e4ae6a91c4084327539af02fbb67a6be',
+        'https://github.com/trilogy-group/gfi-endpointsecurity' : '',
+        'https://github.com/trilogy-group/gfi-eventsmanager' : '',
+        'https://github.com/trilogy-group/gfi-faxmaker-online' : '',
+        'https://github.com/trilogy-group/gfi-faxmaker-server' : '',
+        'https://github.com/trilogy-group/gfi-languard' : '',
+        'https://github.com/trilogy-group/gfi-mail-essentials' : '',
+        'https://github.com/trilogy-group/gfi-oneconnect-M1-EMSClient' : '1654eb63f28a6eda90f0389e75942ef9896f787f',
+        'https://github.com/trilogy-group/gfi-oneguard' : '',
+        'https://github.com/trilogy-group/gfi-web-monitor' : '',
+        'https://scm.devfactory.com/stash/scm/aurea-dfi/dotnet-importer' : '',
+        'https://github.com/trilogy-group/versata-m1.client-ems-client' : '20683b4d4879f207261a58812da8c67e06dde861',
+        'https://scm.devfactory.com/stash/scm/upland-filebound/leadlander.git' : '',
+        'https://scm.devfactory.com/stash/scm/upland-filebound/filebound_v7' : '0624a74f720b049b47c038f35c0c4eba08432897',
+        'https://scm.devfactory.com/stash/scm/upland/filebound_v7' : '923bde470f7f3b89a3fbf5faa456e598a3a7dcec',
+        'https://scm.devfactory.com/stash/scm/upland-filebound/projectlviv' : '31bb6194ad1bbfa9fd4bc2095729301a0a8c5879',
+        'https://scm.devfactory.com/stash/scm/upland-filebound/filebound_capture_v7' : '4640619f9fa92310c40f85696a9c741beff7c240',
+        'https://scm.devfactory.com/stash/scm/UPLAND-ECLIPSE/eclipse-tfs-2-git' : '',
+        'https://github.com/trilogy-group/vepmlive-epmlive2013release' : '',
+        'https://github.com/johnsonz/MvcContosoUniversity' : '',
+        'https://github.com/NancyFx/Nancy' : 'b25b06e33d76da17bea1cabbfd29324ee4ee84d8',
+        'https://github.com/zeromq/netmq' : '',
+        'https://github.com/JamesNK/Newtonsoft.Json' : '',
+        'https://github.com/nhibernate/nhibernate-core' : '',
+        'https://github.com/nunit/nunit' : '',
+        'https://github.com/restsharp/RestSharp' : '',
+        'https://github.com/markrendle/Simple.Data' : '',
+        'https://github.com/danielpalme/MVCBlog' : '',
+        'https://github.com/JoshClose/CsvHelper' : '',
+        'https://github.com/Reactive-Extensions/Rx.NET' : '',
+        'https://github.com/NSwag/NSwag' : '',
+        'https://github.com/jagregory/fluent-nhibernate' : '',
+        'https://github.com/mongodb/mongo-csharp-driver' : '24fd51c3ac703a1da6f88e0bbd9bd55b7048d632',
+        'https://github.com/paulcbetts/refit' : '',
+        'https://github.com/paulcbetts/ModernHttpClient' : '',
+        'https://github.com/ravendb/ravendb' : '',
+        'https://github.com/mbdavid/LiteDB' : '75d42bf845679a18c37670f3d728daae283114b5',
+        'https://github.com/aspnet/Microsoft.Data.Sqlite' : '',
+        'https://github.com/aspnet/EntityFramework6' : '',
+        'https://github.com/aspnet/Razor' : 'ebe8ab4612b6cf4abb63a59993cab6b16bfd05ba',
+        'https://github.com/aspnet/dotnet-watch' : '',
+        'https://github.com/aspnet/MusicStore' : '',
+        'https://github.com/aspnet/NerdDinner' : '',
+        'https://github.com/NancyFx/Nancy.Blog' : '',
+        'https://github.com/NancyFx/Nancy.Demo.Samples' : '',
+        'https://github.com/AvaloniaUI/Avalonia' : '',
+        'https://github.com/StackExchange/dapper-dot-net' : '',
+        'https://github.com/AutoMapper/AutoMapper' : '',
+        'https://github.com/Redth/PushSharp' : '',
+        'https://github.com/PavelTorgashov/FastColoredTextBox' : '',
+        'https://github.com/SignalR/SignalR' : '',
+        'https://github.com/opserver/Opserver' : '',
+        'https://github.com/reactiveui/ReactiveUI' : '',
+        'https://github.com/cefsharp/CefSharp' : '',
+        'https://github.com/NLog/NLog' : '1d6aa87e57d8f5781e3e77efcf751109560e5b90',
+        'https://github.com/HangfireIO/Hangfire' : '',
+        'https://github.com/FransBouma/Massive' : '',
+        'https://github.com/JeremySkinner/FluentValidation' : '',
+        'https://github.com/StackExchange/StackExchange.Redis' : '',
+        'https://github.com/quartznet/quartznet' : '',
+        'https://github.com/Topshelf/Topshelf' : '',
+        'https://github.com/kevin-montrose/Jil' : '',
+        'https://github.com/schambers/fluentmigrator' : '',
+        'https://github.com/akavache/Akavache' : '',
+        'https://github.com/PerfDotNet/BenchmarkDotNet' : '',
+        'https://github.com/Antaris/RazorEngine' : '',
+        'https://github.com/ServiceStack/ServiceStack.Redis' : '',
+        'https://github.com/aspnet/KestrelHttpServer' : '',
+        'https://github.com/OrchardCMS/Orchard' : '',
+        'https://github.com/NEventStore/NEventStore' : '',
+        'https://github.com/jstedfast/MailKit' : '',
+        'https://github.com/MediaBrowser/Emby' : '',
+        'https://github.com/mgravell/protobuf-net' : '',
+        'https://github.com/npgsql/npgsql' : '',
+        'https://github.com/CollaboratingPlatypus/PetaPoco' : '',
+        'https://github.com/EasyNetQ/EasyNetQ' : '',
+        'https://github.com/ServiceStack/ServiceStack.Text' : '',
+        'https://github.com/sjdirect/abot' : '',
+        'https://github.com/madskristensen/MiniBlog' : '',
+        'https://github.com/nopSolutions/nopCommerce' : '',
+        'https://github.com/oxyplot/oxyplot' : '',
+        'https://github.com/MarlabsInc/SocialGoal' : '',
+        'https://github.com/beto-rodriguez/Live-Charts' : '',
+        'https://github.com/smartstoreag/SmartStoreNET' : '',
+        'https://github.com/smsohan/MvcMailer' : '',
+        'https://github.com/StackExchange/NetGain' : '',
+        'https://github.com/fluentscheduler/FluentScheduler' : '',
+        'https://github.com/DotNetAnalyzers/StyleCopAnalyzers' : '',
+        'https://github.com/Knagis/CommonMark.NET' : '',
+        'https://github.com/pascalabcnet/pascalabcnet' : '',
+        'https://github.com/PowerPointLabs/PowerPointLabs' : '',
+        'https://github.com/naudio/NAudio' : '',
+        'https://github.com/apache/logging-log4net' : ''
 ]
+
 
 
 println ">>> SCRIPT STARTED <<<"
@@ -46,7 +150,7 @@ urlCG = 'http://codegraph-api-prod.ecs.devfactory.com/api/1.0/graphs?status=Comp
 println ">>> URL CODEGRAPH: " + urlCG
 println ">>> GMT:" + new Date().toGMTString()
 
-updateDatabase = false; //CHANGE HERE
+updateDatabase = true; //CHANGE HERE
 
 def cgClient = new RESTClient(urlCG)
 cgClient.getClient().params.setParameter("http.connection.timeout", 10000)
@@ -56,8 +160,8 @@ cgProjects.each { sourceUrl, revision ->
     retry(3, { e -> e.printStackTrace() }) {
         cgClient.request(Method.GET) {
             response.success = { resp, json ->
-                //processResponse(json, sourceUrl, revision)
-                processResponseGetOffline(json)
+                processResponse(json, sourceUrl, revision)
+                //processResponseGetOffline(json)
             }
 
             response.failure = { resp ->
@@ -113,6 +217,9 @@ private void processResponse(json, sourceUrl, revision) {
         def neo4jClient = new RESTClient(cgProject['neo4jUrl'])
         neo4jClient.getClient().params.setParameter("http.connection.timeout", 10000)
         neo4jClient.getClient().params.setParameter("http.socket.timeout", 11000)
+
+        //println sourceUrl + "," + cgProject['requestId'] + "," + cgProject['revision']
+
         neo4jClient.request(Method.GET) {
             response.success = { respNeo, jsonNeo ->
                 println ">>> " + sourceUrl + " : " + revision + " : " + cgProject['neo4jUrl'] + " - ONLINE. requestId: " + cgProject['requestId']
