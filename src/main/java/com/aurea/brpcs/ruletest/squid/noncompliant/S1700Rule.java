c96d3639786a3905f1f2554a4d3d335b563fd3d8
Alexandre <ajanoni@gmail.com>
2017-08-08 10:28:51 -0300

    rule S1700

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1700Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1700Rule.java
@@ -2,8 +2,8 @@ package com.aurea.brpcs.ruletest.squid.noncompliant;
 
 public class S1700Rule {
 
-    private String S1700Rule;
+    private String s1700Rule;
 
-    public String getS1700Rule() { return this.S1700Rule; }
+    public String getS1700Rule() { return this.s1700Rule; }
 
 }
