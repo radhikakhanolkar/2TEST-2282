b9c00e5ab0ee43b0af6c22d4a6159e9384a4f0b7
Alexandre <ajanoni@gmail.com>
2017-08-11 17:29:55 -0300

    rule S1700

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1700Rule3.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1700Rule3.java
@@ -2,7 +2,7 @@ package com.aurea.brpcs.ruletest.squid.noncompliant;
 
 public class S1700Rule3 {
 
-    public static class InnerS1700 {
+    private static class InnerS1700 {
 
         private int InnerS1700 = -1;
 
