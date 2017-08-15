3a100ff3d7d70942aba6ed4f1f760b2d3a92eafd
Alexandre <ajanoni@gmail.com>
2017-08-15 09:52:17 -0300

    Rule S2737

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2737Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2737Rule.java
@@ -29,4 +29,24 @@ public class S2737Rule {
         }
         return sb.toString();
     }
+
+    public String readFileCase2(File f) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        FileReader fileReader = null;
+        try {
+            String fileName = "filename";
+            fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            fileReader.close();
+        } catch (IOException e) {
+            throw e;
+        } catch (RuntimeException e) {
+            logger.error("Error log", e);
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
+        return sb.toString();
+    }
 }
