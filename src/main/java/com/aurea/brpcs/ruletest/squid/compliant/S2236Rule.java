88fca78bd3acdd563de11997bef94f54e3b78493
Alexandre <ajanoni@gmail.com>
2018-03-13 21:43:11 -0600

    Improved cases for S2274

--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2236Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2236Rule.java
@@ -1,11 +1,6 @@
 package com.aurea.brpcs.ruletest.squid.compliant;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 public class S2236Rule {
-
-    private static final int TIMEOUT = 2000;
 
     public void testThreadWait() throws InterruptedException {
         Thread myThread = new Thread();
@@ -19,36 +14,4 @@ public class S2236Rule {
         myThread.start();
     }
 
-    public void testThreadWait_Case2() throws InterruptedException {
-        Thread myThread = new Thread();
-        Integer x = 1;
-        synchronized (new Object()) {
-            while (x != 2) {
-                x++;
-                x.wait(TIMEOUT);
-            }
-        }
-        myThread.start();
-    }
-
-    public void testCountDownLatch() throws InterruptedException {
-        CountDownLatch startSignal = new CountDownLatch(1);
-        startSignal.await(1, TimeUnit.MINUTES);
-    }
-
-    public void testNonInheritedWait() {
-        WaitTest wait = new WaitTest();
-        wait.wait("None");
-    }
-
-    class WaitTest {
-
-        public void wait(String param) {
-            doNothing();
-        }
-
-        private void doNothing() {
-            System.out.println("doNothing");
-        }
-    }
 }
--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2274Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2274Rule.java
@@ -1,6 +1,11 @@
 package com.aurea.brpcs.ruletest.squid.compliant;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class S2274Rule {
+
+    private static final int TIMEOUT = 2000;
 
     public void waitTest() throws InterruptedException {
         Integer obj = 1;
@@ -21,6 +26,39 @@ public class S2274Rule {
                 obj++;
                 obj.wait(1000);
             } while (obj != 2);
+        }
+    }
+
+    public void testThreadWait_Case2() throws InterruptedException {
+        Thread myThread = new Thread();
+        Integer x = 1;
+        synchronized (new Object()) {
+            while (x != 2) {
+                x++;
+                x.wait(TIMEOUT);
+            }
+        }
+        myThread.start();
+    }
+
+    public void testCountDownLatch() throws InterruptedException {
+        CountDownLatch startSignal = new CountDownLatch(1);
+        startSignal.await(1, TimeUnit.MINUTES);
+    }
+
+    public void testNonInheritedWait() {
+        WaitTest wait = new WaitTest();
+        wait.wait("None");
+    }
+
+    class WaitTest {
+
+        public void wait(String param) {
+            doNothing();
+        }
+
+        private void doNothing() {
+            System.out.println("doNothing");
         }
     }
 
