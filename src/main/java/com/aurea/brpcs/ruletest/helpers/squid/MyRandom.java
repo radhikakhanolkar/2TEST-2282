aa5bc89e4197c5acf7488eab247dd1b34d9f246d
ekovaleva <ekaterina.kovaleva@aurea.com>
2018-03-15 16:24:03 +0300

    Added more cases for s2245

---
+++ src/main/java/com/aurea/brpcs/ruletest/helpers/squid/MyRandom.java
@@ -0,0 +1,7 @@
+package com.aurea.brpcs.ruletest.helpers.squid;
+
+import java.util.Random;
+
+public class MyRandom extends Random {
+
+}
--- src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2245Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/compliant/S2245Rule.java
@@ -1,9 +1,16 @@
 package com.aurea.brpcs.ruletest.squid.compliant;
 
+import java.security.SecureRandom;
 import com.aurea.brpcs.ruletest.helpers.squid.Random;
 
 public class S2245Rule {
 
     private final Random r = new Random(12);
+
+    public void doSomething() {
+        java.util.Random random = new SecureRandom();
+        byte bytes[] = new byte[20];
+        random.nextBytes(bytes);
+    }
 
 }
