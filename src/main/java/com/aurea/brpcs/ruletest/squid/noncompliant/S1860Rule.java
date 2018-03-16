a87bd09df8512af54efe3366abb776162f4f1b56
yogeshx <yogesh.mahajan@aurea.com>
2018-03-16 19:22:55 +0530

    Updated S1860 - removed invalid code.

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1860Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S1860Rule.java
@@ -1,44 +1,73 @@
-package com.aurea.brpcs.ruletest.findbugs.noncompliant;
+package com.aurea.brpcs.ruletest.squid.noncompliant;
 
+public class S1871Rule {
 
-public class S1860Rule{
+	public void anIfMethod(int a) {
 
-    public void simpleCalls(){
-        complaint();
-        nonComplaint();
-        nonComplaint2();
-        nonComplaint3();
-    }
+		if (a >= 0 && a < 10) {
+			doFirstThing();
+			doTheThing();
+		} else if (a >= 10 && a < 20) {
+			doTheOtherThing();
+		} else if (a >= 20 && a < 50) {
+			doFirstThing();
+			doTheThing(); // Noncompliant; duplicates first condition
+		} else {
+			doTheRest();
+		}
+	}
 
-    private void complaint() {
-        Object lock;
-        synchronized (lock)
-        {
-            System.out.println("complaint()");
-        }
-    }
+	private void doTheOtherThing() {
+		// TODO Auto-generated method stub
 
-    void nonComplaint() {
-        int lock;
-        synchronized (lock)
-        {
-            System.out.println("nonComplaint()");
-        }
-    }
+	}
 
-    public void nonComplaint2() {
-            Integer lock;
-            synchronized (lock)
-            {
-                System.out.println("nonComplaint2()");
-            }
-        }
+	private void doTheThing() {
+		// TODO Auto-generated method stub
 
+	}
 
-    public void nonComplaint3 () {
-            java.lang.Integer lock;
-            synchronized (lock) {
-                System.out.println("nonComplaint3()");
-            }
-        }
+	public void aSwitchMethod(int i) {
+
+		switch (i) {
+		case 1:
+			doFirstThing();
+			doSomething();
+			break;
+		case 2:
+			doSomethingDifferent();
+			break;
+		case 3: // Noncompliant; duplicates case 1's implementation
+			doFirstThing();
+			doSomething();
+			break;
+		case 4:
+			doSomethingDifferent();  // compliant; Only 1 implementation line (case 2)
+			break;
+		default:
+			doTheRest();
+		}
+
+	}
+
+	private void doTheRest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doSomethingDifferent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doSomething() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doFirstThing() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
