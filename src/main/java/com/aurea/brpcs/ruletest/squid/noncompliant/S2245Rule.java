95e6bb1ea83eefbdbd0f43ea4ade88443c913780
ekovaleva <ekaterina.kovaleva@aurea.com>
2018-03-19 18:34:40 +0100

    case for s2245

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2245Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2245Rule.java
@@ -6,6 +6,7 @@ import com.aurea.brpcs.ruletest.helpers.squid.MyRandom;
 public class S2245Rule {
 
     private static final Random myGenerator = new Random(System.currentTimeMillis());
+    private static final Random rnd = new Random();
 
     private Random r = new java.util.Random();
     private Random r2 = new Random(12);
