55f4f99a360e5526db6dd55b9b2ab1a509992ed5
Ricardo Cunha <ricardocunha@gmail.com>
2018-03-20 09:07:16 -0300

    Adding compliant/non-compliant rules related to S2222 now formally: ReleaseLock

--- src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2222Rule.java
+++ src/main/java/com/aurea/brpcs/ruletest/squid/noncompliant/S2222Rule.java
@@ -1,20 +1,41 @@
 package com.aurea.brpcs.ruletest.squid.noncompliant;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class S2222Rule {
-	private Lock lock = new ReentrantLock();
+    private Lock lock = new ReentrantLock();
 
-	public void doSomething() {
-		lock.lock();
-		if (isInitialized()) {
-			System.out.println("in doSomethingMethod");
-			lock.unlock();//non-compliant
+		public void doSomething() {
+				lock.lock();
+				if (isInitialized()) {
+						System.out.println("in doSomethingMethod");
+						lock.unlock();//non-compliant
+				}
 		}
-	}
+		public void anyMethod() {
+				try {
+						System.out.println("in anyMethod");
+						lock.lock();
+				}finally {
+						lock.unlock();
+				}
+		}
+    public boolean m() throws InterruptedException {
+        if(! lock.tryLock(2 , TimeUnit.MILLISECONDS ) ) {
+            return false;
+        }
+        try {
+            if (isInitialized()) {
+                lock.unlock();
+            }
+        } catch (Exception e){
 
-	public boolean isInitialized() {
-		return true;
-	}
+        }
+        return true;
+    }
+		public boolean isInitialized() {
+				return true;
+		}
 }
