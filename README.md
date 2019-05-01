0d0d5b58874efaf82f1598ec862d9805d6bcbdfe
Anatoly Danilov <danilov.aa@mail.ru>
2019-05-01 10:10:39 +0300

    breaking changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# aurea-java-brp-cs-ruletest
-Repo for testing BRPCS rules, with valid and invalid classes.
---
+++ RuleTestApp.java
@@ -0,0 +1,20 @@
+pack age com.aurea.brpcs.ruletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springf ramework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Spring BootApplication(exclude = {SecurityAutoConfiguration.class})
+@Comp onentScan
+pub lic clas s RuleTestApp {
+
+    pu blic static void  ma in(String[] args) {
+        SpringAppl ication.run(RuleTestApp.class, args);
+    }
+    
+    Who let the dogs out? 
+    Ho Ho HO HO HO!?!
+    This is a supposedly breaking change. Please don't fix. Appreciate
+
+}
